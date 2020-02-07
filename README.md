45ee5faf2dab984d18c840c9b55872b6c396b3e1
JelteMX <jelte.lagendijk@mendix.com>
2020-02-07 16:05:18 +0100

    :fire: First setup

---
+++ README.md
@@ -0,0 +1,41 @@
+[![Support](https://img.shields.io/badge/Support-Community%20(no%20active%20support)-orange.svg)](https://docs.mendix.com/developerportal/app-store/app-store-content-support)
+[![Studio](https://img.shields.io/badge/Studio%20version-8.0%2B-blue.svg)](https://appstore.home.mendix.com/link/modeler/)
+![GitHub release](https://img.shields.io/github/release/JelteMX/mendix-jsontotree-module)
+![GitHub issues](https://img.shields.io/github/issues/JelteMX/mendix-jsontotree-module)
+
+# JSON To Tree Module for Mendix
+
+![Icon](/assets/AppStoreIcon.png)
+
+You have a simple JSON that has a nested structure (children) and you want to import this into Mendix?
+
+## Java Actions
+
+- Convert JSON to Mendix Object Tree (non-persistent)
+
+This will create a tree of non-persistent Node objects that each have a parent, and are bound to a Tree object. See Domain-model:
+
+![Domain](/assets/DomainModel.png)
+
+How you follow up with that is up to you. The module includes a few examples of how you can create a Tree structure. This is showcased in the test-project:
+
+[https://treecreatortestapp-sandbox.mxapps.io/](https://treecreatortestapp-sandbox.mxapps.io/)
+
+## Libraries used
+
+- Jackson Core
+    - Version 2.10.2
+    - License: Apache 2.0
+    - [link](https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-core/2.10.2)
+- Jackson Annotations
+    - Version 2.10.2
+    - License: Apache 2.0
+    - [link](https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-annotations/2.10.2)
+- Jackson Databind
+    - Version 2.10.2
+    - License: Apache 2.0
+    - [link](https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind/2.10.2)
+
+## License
+
+Apache 2
---
+++ src/javasource/jsontotree/Misc.java
@@ -0,0 +1,167 @@
+package jsontotree;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
+import com.mendix.core.Core;
+import com.mendix.core.CoreException;
+import com.mendix.systemwideinterfaces.core.IContext;
+import com.mendix.systemwideinterfaces.core.IMendixObject;
+import com.mendix.thirdparty.org.json.JSONObject;
+import jsontotree.proxies.JSONNode;
+
+public class Misc {
+
+	/**
+	 * Traverse a JSON Tree 
+	 * 
+	 * @param ctx Context for the current Java Action
+	 * @param rootObject Root object that binds all Node objects
+	 * @param rootNodes Array/Single Tree object
+	 * @param childKey Key that represents the array with child nodes
+	 */
+	public static void traverseRootObjects(
+			IContext ctx, 
+			IMendixObject rootObject, 
+			JsonNode rootNodes,
+			String childKey
+	) {
+		traverse(rootObject, ctx, rootNodes, childKey, null);
+	}
+
+	/**
+	 * Root traverse mode, will traverse through an array or single object
+	 * 
+	 * @param rootObject Root object that binds all Node objects
+	 * @param ctx Context for the current Java Action
+	 * @param node current node
+	 * @param childKey Key that represents the array with child nodes
+	 * @param parentMXObject Mendix JSONNode object that is the parent
+	 */
+	private static void traverse(IMendixObject rootObject, IContext ctx, JsonNode node, String childKey, IMendixObject parentMXObject) {
+		if (node.getNodeType() == JsonNodeType.ARRAY) {
+			traverseArray(rootObject, ctx, node, childKey, parentMXObject);
+		} else if (node.getNodeType() == JsonNodeType.OBJECT) {
+			traverseObject(rootObject, ctx, node, childKey, parentMXObject);
+		} else {
+			throw new com.mendix.systemwideinterfaces.MendixRuntimeException("Problem with traversing JSON, node type not implemented yet: " + node.getNodeType().toString());
+		}
+	}
+
+	/**
+	 * Traverse through single node
+	 * 
+	 * @param rootObject Root object that binds all Node objects
+	 * @param ctx Context for the current Java Action
+	 * @param node current node
+	 * @param childKey Key that represents the array with child nodes
+	 * @param parentMXObject Mendix JSONNode object that is the parent
+	 */
+	private static void traverseObject(IMendixObject rootObject, IContext ctx, JsonNode node, String childKey, IMendixObject parentMXObject) {
+		JsonNode child = node.get(childKey);
+		boolean hasChild = traversable(child);
+		JSONObject copy = createJSONObject(node, childKey);
+		String jsonContent = copy.toString();
+		
+		IMendixObject jsonObj = createNodeObject(ctx, rootObject, parentMXObject, jsonContent);
+
+		if (hasChild) {
+			traverse(rootObject, ctx, child, childKey, jsonObj);
+		}
+	}
+
+	/**
+	 * Traverse through array of nodes 
+	 * 
+	 * @param rootObject Root object that binds all Node objects
+	 * @param ctx Context for the current Java Action
+	 * @param node current node
+	 * @param childKey Key that represents the array with child nodes
+	 * @param parentMXObject Mendix JSONNode object that is the parent
+	 */
+	private static void traverseArray(IMendixObject rootObject, IContext ctx, JsonNode node, String childKey, IMendixObject parentMXObject) {
+		for (JsonNode jsonArrayNode : node) {
+			if (traversable(jsonArrayNode)) {
+				traverse(rootObject, ctx, jsonArrayNode, childKey, parentMXObject);
+			}
+		}
+	}
+
+	/**
+	 * Check if the node is not empty, not an object (we don't allow that), but an array
+	 * 
+	 * @param node
+	 * @return Is this a traversable array?
+	 */
+	private static boolean traversable(JsonNode node) {
+		return !node.isEmpty()
+				&& (node.getNodeType() == JsonNodeType.OBJECT || node.getNodeType() == JsonNodeType.ARRAY);
+
+	}
+	
+	/**
+	 * Create a JSON object that holds all the fields from the node, with the exception of the children
+	 * 
+	 * @param node
+	 * @param childKey
+	 * @return
+	 */
+	private static JSONObject createJSONObject(JsonNode node, String childKey) {
+		JSONObject copy = new JSONObject();
+
+		node.fieldNames().forEachRemaining((String fieldName) -> {
+			JsonNode childNode = node.get(fieldName);
+
+			if (!fieldName.equalsIgnoreCase(childKey)) {
+				Object value = null;
+				if (childNode.isTextual()) {
+					value = childNode.textValue();
+				} else if (childNode.isNumber()) {
+					value = childNode.numberValue();
+				} else if (childNode.isDouble()) {
+					value = childNode.doubleValue();
+				} else if (childNode.isLong()) {
+					value = childNode.asLong();
+				} else if (childNode.isBoolean()) {
+					value = childNode.asBoolean();
+				} else {
+					value = childNode.asText();
+				}
+
+				copy.put(fieldName, value);
+			}
+		});
+		
+		return copy;
+	}
+	
+	/**
+	 * Create a Mendix JSONNode object
+	 * 
+	 * @param ctx
+	 * @param rootObject
+	 * @param parentMXObject
+	 * @param jsonContent
+	 * @return
+	 */
+	private static IMendixObject createNodeObject(IContext ctx, IMendixObject rootObject, IMendixObject parentMXObject, String jsonContent) {
+		IMendixObject JSONRepresentationObject = Core.instantiate(ctx, JSONNode.getType());
+
+		JSONRepresentationObject.setValue(ctx, JSONNode.MemberNames.Content.toString(),
+				jsonContent);
+		JSONRepresentationObject.setValue(ctx, JSONNode.MemberNames.JSONNode_Root.toString(),
+				rootObject.getId());
+		
+		if (parentMXObject != null) {
+			JSONRepresentationObject.setValue(ctx, JSONNode.MemberNames.Parent.toString(), parentMXObject.getId());
+		}
+
+		try {
+			Core.commit(ctx, JSONRepresentationObject);
+		} catch (CoreException e) {
+			throw new com.mendix.systemwideinterfaces.MendixRuntimeException("Issue with committing temporary JSON Object");
+		}
+		
+		return JSONRepresentationObject;
+	}
+
+}
